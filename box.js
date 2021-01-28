19d857af0bca8b14b7e051854b56a8c5f1270798
Mannangoyal3 <75422299+Mannangoyal3@users.noreply.github.com>
2021-01-28 16:13:23 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,31 @@
+class box{
+
+constructor(x,y,width,height){
+var options = {
+restitution:  1,
+fricton: 0.7,
+density: 1
+
+
+}
+this.body = Bodies.rectangle(x,y,width,height,options);
+this.width = width;
+this.height = height;
+World.add(world,this.body);
+}
+
+display(){
+var position = this.body.position;
+var angle = this.body.angle;
+push();
+translate(position.x,position.y);
+rotate(angle);
+rectMode(CENTER);
+fill("cyan");
+stroke("blue");
+rect(0,0,this.width,this.height);
+pop();
+
+
+}
+}
